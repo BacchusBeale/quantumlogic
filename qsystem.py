dbd71244e0819cebcadef421146218b55c8b3c6a
Bacchus Beale <bealebacchus@gmail.com>
2023-06-08 19:48:31 +1000

    save

--- qsystem.py
+++ qsystem.py
@@ -11,106 +11,30 @@ class QComputer():
         self.numQubits = numQubits
         self.numBits = numQubits
         if numBits<1:
-            self.qCircuit = QuantumCircuit(self.numQubits)
+            self.circuit = QuantumCircuit(self.numQubits)
         else:
-            self.qCircuit = QuantumCircuit(self.numQubits, self.numBits)
+            self.circuit = QuantumCircuit(self.numQubits, self.numBits)
 
-        self.instructionSet = None
-        self.stateVector = None
-        self.measurementCircuit = None
-        self.resultCircuit = None
-
-    def showDataArray(self):
-        print("showDataArray")
-        U = Operator(self.qCircuit)
-        print(U.data)
-
-    def updateStateVector(self, useResultCircuit=False):
-        print("update state vector")
-        # Set the intial state of the simulator to the ground state using from_int
-        self.stateVector = Statevector.from_int(0, 2**3)
-        # Evolve the state by the quantum circuit
-        if useResultCircuit==False:
-            self.stateVector = self.stateVector.evolve(self.qCircuit)
-        else:
-            if self.resultCircuit is None:
-                return False
-            
-            self.stateVector = self.stateVector.evolve(self.resultCircuit)
-
-        return True
-
-    # Valid choices are qsphere, hinton, bloch, city, or paulivec.
-    def showStateVectorPlot(self, drawType='qsphere', show=False, saveAs=None):
-        print(f"show state vector: {drawType}")
-        if self.stateVector is None:
-            return False
-        self.stateVector.draw(output=drawType)
+    def resetQubit(self, qubitIndex=0):
+        self.circuit.reset(qubitIndex)
         
-        if saveAs:
+    def addNotGate(self, qubitIndex=0):
+        self.circuit.x(qubitIndex)
+
+    def addBarrier(self):
+        self.circuit.barrier()
+
+    def addMeasure(self, qubitIndex, bitIndex):
+        self.circuit.measure(qubitIndex, bitIndex)
+
+    def plotCircuit(self, display=True, saveFile=False, saveAs='circuit.png'):
+        self.circuit.draw('mpl')
+        if display:
+            plt.show()
+
+        if saveFile:
             plt.savefig(saveAs)
 
-        if show:
-            plt.show()
-        return True
-    
-    # Valid choices are text, repr
-    def getStateVectorText(self, useRepr=True, saveAs=None):
-        print("getStateVectorText")
-        if self.stateVector is None:
-            return False
-        
-        result = ''
-        if useRepr:
-            result = self.stateVector.draw() # repr is default
-        else:
-            result = self.stateVector.draw(output='text')
 
-        if saveAs:
-            with open(saveAs, 'w') as f:
-                f.write(result)
-        return True
 
-    def addHadamardGate(self, qubitNumber):
-        print("Add H gate")
-        self.instructionSet = self.qCircuit.h(qubit=qubitNumber)
-        return self.instructionSet
-    
-    def addCNotGate(self, qubitFrom, qubitTo):
-        print("Add C Not gate")
-        self.instructionSet = self.qCircuit.cnot(
-            control_qubit=qubitFrom, 
-            target_qubit=qubitTo)
-        return self.instructionSet
-    
-    def addBarrier(self, numQubits):
-        self.qCircuit.barrier(range(numQubits))
-
-    def addMeasurement(self, numQubits, numBits):
-        print(f"addMeasurement: {numQubits}, {numBits}")
-        self.measurementCircuit = QuantumCircuit(numQubits, numBits)
-        self.measurementCircuit.measure(range(numQubits), range(numBits))
-        self.resultCircuit = self.measurementCircuit.compose(
-            self.qCircuit, range(numBits), front=True
-        )
-    
-    def visualiseCircuit(self, showWindow=True, saveAs=None):
-        print("show circuit")
-        if saveAs:
-            self.qCircuit.draw('mpl', filename=saveAs)
-        else:
-            self.qCircuit.draw('mpl')
-
-        if showWindow:
-            plt.show()
-
-    def visualiseResultCircuit(self, showWindow=True, saveAs=None):
-        print("show circuit")
-        if saveAs:
-            self.resultCircuit.draw('mpl', filename=saveAs)
-        else:
-            self.resultCircuit.draw('mpl')
-
-        if showWindow:
-            plt.show()
         
--- test.py
+++ test.py
@@ -1,114 +1,23 @@
 import qsystem
-import matplotlib.pyplot as plt
 
-def testGHZCircuit():
-    nQubits=3
-    nBits=0
-    computer = qsystem.QComputer(
-        numQubits=nQubits,
-        numBits=nBits
+def makeNotGate():
+    nQubits=1
+    nBits=1
+    qc = qsystem.QComputer(
+        numBits=nBits,
+        numQubits=nQubits
     )
 
-    handle = computer.addHadamardGate(
-        qubitNumber=0
-    )
-    handle = computer.addCNotGate(
-        qubitFrom=0,
-        qubitTo=1
-    )
-
-    handle = computer.addCNotGate(
-        qubitFrom=0,
-        qubitTo=2
+    qc.resetQubit(qubitIndex=0)
+    qc.addNotGate(qubitIndex=0)
+    qc.addBarrier()
+    qc.addMeasure(qubitIndex=0, bitIndex=0)
+    qc.plotCircuit(
+        display=True,
+        saveAs='notgate.png',
+        saveFile=True
     )
 
-    print(handle)
-    computer.visualiseCircuit(
-        showWindow=True,
-        saveAs='ghzstate.png'
-    )
 
-    computer.visualiseResultCircuit(
-        showWindow=True,
-        saveAs='ghzmeasure.png'
-    )
 
-    computer.updateStateVector(useResultCircuit=False)
-
-    asciiTxt = computer.getStateVectorText(
-        useRepr=True,
-        saveAs='staterepr.txt'
-    )
-
-    print("State vector repr\n")
-    print(asciiTxt)
-    print("==================\n")
-
-    asciiTxt = computer.getStateVectorText(
-        useRepr=False,
-        saveAs='statetext.txt'
-    )
-
-    print("State vector text\n")
-    print(asciiTxt)
-    print("==================\n")
-
-    computer.showStateVectorPlot(
-        drawType='qsphere',
-        show=True,
-        saveAs='qsphere.png'
-    )
-
-    computer.showStateVectorPlot(
-        drawType='bloch',
-        show=True,
-        saveAs='bloch.png'
-    )
-
-    computer.showStateVectorPlot(
-        drawType='paulivec',
-        show=True,
-        saveAs='paulivec.png'
-    )
-
-    # Measure
-    computer.addBarrier()
-    computer.addMeasurement(
-        numQubits=nQubits,
-        numBits=nBits
-    )
-    # AFTER MEASUREMENT
-
-    computer.updateStateVector(useResultCircuit=False)
-
-    asciiTxt = computer.getStateVectorText(
-        useRepr=True,
-        saveAs='staterepr.txt'
-    )
-
-    print("State vector repr\n")
-    print(asciiTxt)
-    print("==================\n")
-
-    asciiTxt = computer.getStateVectorText(
-        useRepr=False,
-        saveAs='statetext.txt'
-    )
-
-    print("State vector text\n")
-    print(asciiTxt)
-    print("==================\n")
-
-    computer.showStateVectorPlot(
-        drawType='qsphere',
-        show=True,
-        saveAs='qsphere.png'
-    )
-
-    computer.showStateVectorPlot(
-        drawType='bloch',
-        show=True,
-        saveAs='bloch.png'
-    )
-
-testGHZCircuit()
+makeNotGate()
